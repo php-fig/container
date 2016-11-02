ea053f9d8e6b73f7b199734f5f9be64f07831e61
Matthieu Napoli <matthieu@mnapoli.fr>
2016-11-02 22:56:39 +0100

    Move exceptions to the main namespace
    This is so that we are consistent with other PSRs.
    
    https://groups.google.com/forum/#!topic/php-fig/L8rDUwRFsOU

--- src/ContainerException.php
+++ src/ContainerException.php
@@ -3,7 +3,7 @@
  * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
  */
 
-namespace Psr\Container\Exception;
+namespace Psr\Container;
 
 /**
  * Base interface representing a generic exception in a container.
--- src/NotFoundException.php
+++ src/NotFoundException.php
@@ -3,7 +3,7 @@
  * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
  */
 
-namespace Psr\Container\Exception;
+namespace Psr\Container;
 
 /**
  * No entry was found in the container.
