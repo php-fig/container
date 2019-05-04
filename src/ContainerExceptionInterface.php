109d59306dd9aad13faa8aade99fb23a1d2183e0
Erik Booij <erik@booij.me>
2019-05-04 14:14:04 +0200

    Make ContainerExceptionInterface extend Throwable where it is available

--- src/ContainerExceptionInterface.php
+++ src/ContainerExceptionInterface.php
@@ -2,9 +2,17 @@
 
 namespace Psr\Container;
 
+use Throwable;
+
 /**
  * Base interface representing a generic exception in a container.
  */
-interface ContainerExceptionInterface
-{
+if (interface_exists(Throwable::class)) {
+    interface ContainerExceptionInterface extends Throwable
+    {
+    }
+} else {
+    interface ContainerExceptionInterface
+    {
+    }
 }
