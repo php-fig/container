19764b1677626ba3db35c033c329f9d603e87f26
Alessandro Lai <alessandro.lai85@gmail.com>
2021-03-08 10:32:22 +0100

    Remove Throwable interface from exceptions (port of #31 to 2.x)

--- src/NotFoundExceptionInterface.php
+++ src/NotFoundExceptionInterface.php
@@ -5,6 +5,6 @@ namespace Psr\Container;
 /**
  * No entry was found in the container.
  */
-interface NotFoundExceptionInterface extends ContainerExceptionInterface
+interface NotFoundExceptionInterface
 {
 }
